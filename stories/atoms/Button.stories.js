c140591ef2c0c88c6497c215192b22662ea642f8
FranciscoImanolSuarez <imanolsuarez98@gmail.com>
2021-01-11 20:39:43 -0300

    add atom icon

--- stories/atoms/Button.stories.js
+++ stories/atoms/Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from '../../atomic/atoms/Button'
-
+import Icon from '../../atomic/atoms/Icon'
 export default {
     title: 'Atoms/Button',
     component: Button
@@ -12,3 +12,5 @@ export const Primary = () => (<Button type="primary">Button Primary</Button>)
 export const Secondary = () => (<Button type="secondary">Button Secondary</Button>)
 
 export const Tertiary = () => (<Button type="tertiary">Button Tertiary</Button>)
+
+export const WithIcon = () => (<Button type="secondary">Button Icon <Icon type="arrow-right-solid"/></Button>)
---
+++ stories/atoms/Icon.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Icon from '../../atomic/atoms/Icon'
+
+export default {
+    title: 'Atoms/Icon',
+    component: Icon
+}
+
+
+export const Default = () => (<Icon type="right-arrow"> Icon </Icon>)
